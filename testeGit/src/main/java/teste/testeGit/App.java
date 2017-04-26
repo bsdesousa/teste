5fca76e2cf4f1ec1c28b764d71424199251579a9
bruno.sousa <bruno.sousa@NT-REDOME03.FdoC.Local>
2017-04-26 14:08:59 -0300

    Commit with sync

--- testeGit/src/main/java/teste/testeGit/App.java
+++ testeGit/src/main/java/teste/testeGit/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("commit 1");
+        System.out.println("commit 2");
+        System.out.println("commit 3");
     }
 }
