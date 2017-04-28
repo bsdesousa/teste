3e629dd5d0127469e37edb5a408e047144e5afaf
bsdesousa <bsdesousa@gmail.com>
2017-04-28 10:08:51 -0300

    Mesclar eclipse

--- testeGit/src/main/java/teste/testeGit/App.java
+++ testeGit/src/main/java/teste/testeGit/App.java
@@ -9,10 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("commit 8");
-        System.out.println("commit 9");
-        System.out.println("commit 10");
-        System.out.println( "teste de conflito!" );
+        System.out.println("Usando o mesclar do git no eclipse");
         
     }
 }
