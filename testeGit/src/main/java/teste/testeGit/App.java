aa08be8a4e918c11e58d86b94b84869f8ca0a769
Bruno Sousa <bsdesousa@gmail.com>
2017-05-22 17:21:33 -0300

    Meu commit

--- testeGit/src/main/java/teste/testeGit/App.java
+++ testeGit/src/main/java/teste/testeGit/App.java
@@ -10,6 +10,9 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Usando o mesclar do git no eclipse");
+
+
+        System.out.println("Clonando");
         
     }
 }
