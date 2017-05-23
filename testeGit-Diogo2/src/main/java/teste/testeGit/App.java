7c9975d6d83ce9847ca652c9bc01f4fc86c5e5fd
Pizão <rafael.magna@cancer.org.br>
2017-05-23 09:52:51 -0300

    Causando merge.

--- testeGit-Diogo2/src/main/java/teste/testeGit/App.java
+++ testeGit-Diogo2/src/main/java/teste/testeGit/App.java
@@ -9,8 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		System.out.println("Simulando o caos");
         System.out.println("Usando o mesclar do git no eclipse");
-        System.out.println("Alteração Pizão é o menino dourado");
-        
+		System.out.println("Alteração Pizão");
+        System.out.println("Alteração Pizão 2");
     }
 }
